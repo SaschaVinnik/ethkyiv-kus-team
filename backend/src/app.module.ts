9d4f08216c14134470a77a1172fb7bb3c033763d
Orest Khomitskyi <orestkhomitskyi@gmail.com>
2025-06-14 19:25:08 +0300

    heroku

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,11 +14,10 @@ import { DisputesModule } from './disputes/disputes.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: [UserModel, DisputeModel],
       synchronize: true,
     }),
