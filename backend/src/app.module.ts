e3f0f12ae822eff53f8da1fa8e0580f2cd95d25d
Orest Khomitskyi <orestkhomitskyi@gmail.com>
2025-06-14 19:49:14 +0300

    users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,10 +14,11 @@ import { DisputesModule } from './disputes/disputes.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [UserModel, DisputeModel],
       synchronize: true,
     }),
